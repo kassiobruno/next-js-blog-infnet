a0a9a457102d1726f4938a6ebc2fe887b11b5156
kassiobruno <kaassio1@gmail.com>
2022-12-13 12:22:46 -0300

    Push to repository

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
+            <a>
+              <button>
+                COMEÇAR AGORA!
+              </button>
+            </a>
+            
+          </section>
+
+          <img src='/images/banner-conteudos.png' alt='Conteúdos'/>
+
+        </div>
+
+      </main>
+    </>
+  )
+}
